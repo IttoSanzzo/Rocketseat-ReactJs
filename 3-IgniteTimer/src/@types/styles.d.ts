139c138abc9385c2cc4db41f6f7179856dfda3ce
IttoSanzzo <konohittos@gmail.com>
2025-02-21 06:38:30 -0300

    Ignite Timer Commit 1

---
+++ 3-IgniteTimer/src/@types/styles.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+import { defaultTheme } from "../styles/themes/default";
+import { DefaultTheme } from "styled-components";
+
+type ThemeType = typeof defaultTheme;
+
+declare module "styled-components" {
+	export interface DefaultTheme extends ThemeType {}
+}
